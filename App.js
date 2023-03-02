04e7089a0c3b4a032d2d773905248163882435a6
myzsk <myzskdev@gmail.com>
2023-03-03 01:02:58 +0700

    delete comment

--- App.js
+++ App.js
@@ -5,9 +5,5 @@ export default function App() {
     <NativeBaseProvider>
       <Box>Hello world</Box>
     </NativeBaseProvider>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
   );
 }
