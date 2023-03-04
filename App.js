c9f01bbfe317722ecff34db2b4f7f4a3078a028c
myzsk <myzskdev@gmail.com>
2023-03-04 22:03:23 +0700

    map

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import { NativeBaseProvider, Box } from "native-base";
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+import { NativeBaseProvider, Box, Center } from "native-base";
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box>Hello world</Box>
+      <Center flex={1}>
+        <MapView style={styles.map} />
+      </Center>
     </NativeBaseProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
